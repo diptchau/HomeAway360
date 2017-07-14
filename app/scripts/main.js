9c677e0e9de65aa2d2f21b1a01f9649e55c212f6
Dipt Chaudhary <dchaudhary@homeaway.com>
2017-07-14 18:15:16 -0500

    panoreal

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,21 +1,88 @@
-var panorama, viewer;
+var footer = $('.footer');
+var main = $('main');
+var normalImgHolder = $('#maincontainer');
+var btnBook = $('#book-btn');
+var viewHolderJ = $('#viewbox-360');
+var viewHolder = document.getElementById( 'viewbox-360' );
+var panorama;
 
-var mainContainer = document.getElementById( 'maincontainer' );
+var isMobile = {
+	    Android: function() {
+	        return navigator.userAgent.match(/Android/i);
+	    },
+	    BlackBerry: function() {
+	        return navigator.userAgent.match(/BlackBerry/i);
+	    },
+	    iOS: function() {
+	        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+	    },
+	    Opera: function() {
+	        return navigator.userAgent.match(/Opera Mini/i);
+	    },
+	    Windows: function() {
+	        return navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i);
+	    },
+	    any: function() {
+	        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+	    }
+};
 
-viewer = new PANOLENS.Viewer( { container: mainContainer } );
-panorama = new PANOLENS.ImagePanorama('images/field.jpg');
+
+
+var viewer = new PANOLENS.Viewer({ 
+		container: viewHolder,
+		controlBar: true,
+		cameraFov: 120
+	});
+
+// viewHolderJ.hide();
 
 
 
-$('#maincontainer').click(function(){
-	$('.footer').addClass('img-360-footer');
-	$('main').addClass('main-hide');
-	console.log('trigger');
-	// viewer.add (panorama);
-	setTimeout(function(){
-		$('main').removeClass('main-hide');
-		$('.footer').removeClass('img-360-footer');
-	}, 2000);
+normalImgHolder.click(function() {
+    footer.addClass('img-360-footer');
+    main.addClass('main-hide');
+    normalImgHolder.css('height', '100vh');
+    
+    setTimeout(function(){
+    	main.hide();
+    	Show360pic();
+    	normalImgHolder.css('position', 'absolute');
+    }, 150);
+
 });
 
+btnBook.click(function(){
+	BacktoPDP();
+});
 
+function Show360pic(){	
+	panorama = new PANOLENS.ImagePanorama('images/field.jpg');
+	viewer.add(panorama);
+
+	if(isMobile.any() != null){
+		viewer.enableControl(1);
+	}
+
+	viewHolderJ.removeClass("closed");
+}
+
+function Hide360pic(){
+
+	panorama.dispose();
+    viewer.remove(panorama);
+    panorama = null;
+	viewHolderJ.addClass("closed");
+	
+}
+
+
+function BacktoPDP() {
+	footer.removeClass('img-360-footer');
+	main.show().removeClass('main-hide');
+	normalImgHolder.css({
+    	'height': '60vh',
+    	'position': 'initial'
+    });
+    Hide360pic();
+}
