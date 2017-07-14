b28d5731661b7da118aec8f2d694c598103eea85
Dipt Chaudhary <dchaudhary@homeaway.com>
2017-07-13 20:49:41 -0500

    panostuff

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,34 @@
-console.log('\'Allo \'Allo!');
+
+
+// document.addEventListener("DOMContentLoaded", function() {
+//   var lastElementClicked;
+//   Barba.Pjax.init();
+//   Barba.Prefetch.init();
+// });
+
+// Barba.Dispatcher.on('newPageReady', function(currentStatus, oldStatus, container) {
+// 	if(currentStatus.url.includes('360.html')){
+// 		SetFooterTransparent();
+// 		SetupPanolens();
+// 	}
+// 	else{
+// 		SetFooterPDP();
+// 	}
+// });
+
+// function SetFooterTransparent(){
+// 	$('.footer').addClass('img-360-footer');
+// }
+
+// function SetFooterPDP(){
+// 	$('.footer').removeClass('img-360-footer');
+// }
+
+SetupPanolens();
+
+function SetupPanolens(){
+	var panorama;
+	var viewer = new PANOLENS.Viewer();
+    panorama = new PANOLENS.ImagePanorama( 'images/field.jpg' );
+    viewer.add( panorama );
+}
